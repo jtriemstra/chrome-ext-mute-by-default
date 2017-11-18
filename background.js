beb3e4625f9bab8b41c38a7c03f77834a63ab6f4
Joel Triemstra <jtriemstra@jtriemstraw7.VML.LAN>
2017-11-18 18:29:44 -0500

    manual override working

--- background.js
+++ background.js
@@ -1,24 +1,33 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	const MUTE_KEY = "manualMutes";
+	const MUTE_KEY = 'manualMutes';
 	
 	if (changeInfo.url && "" != changeInfo.url)
 	{
-		if (/msn\.com/.test(changeInfo.url))
-		{
-			chrome.tabs.update(tabId, {"muted":true}, function(tab){})
-		}
-		else
-		{			
-			chrome.tabs.update(tabId, {"muted":false}, function(tab){})
-		}
+		chrome.storage.local.get(MUTE_KEY, function(muteData){
+			var manualMutes = muteData.manualMutes || {};
+			var manuallyMuted = manualMutes["t" + tabId];
+
+			if (manuallyMuted == null)
+			{
+				if (/msn\.com/.test(changeInfo.url))
+				{
+					chrome.tabs.update(tabId, {"muted":true}, function(tab){})
+				}
+				else
+				{			
+					chrome.tabs.update(tabId, {"muted":false}, function(tab){})
+				}
+			}
+		});		
 	}
 	
-	if (changeInfo.mutedInfo)
-	{
-		chrome.storage.local.get(MUTE_KEY, function(data){
-			var manualMutes = data.manualMutes || [];
-			manualMutes["'" + tabId + "'"] = changeInfo.mutedInfo.muted;
-			chrome.storage.local.set({MUTE_KEY:manualMutes});
+	if (changeInfo.mutedInfo && changeInfo.mutedInfo.reason === "user")
+	{		
+		chrome.storage.local.get(MUTE_KEY, function(muteData){
+			var manualMutes = muteData.manualMutes || {};
+			manualMutes["t" + tabId] = changeInfo.mutedInfo.muted;
+			
+			chrome.storage.local.set({[MUTE_KEY]:manualMutes}, function(){console.log(chrome.runtime.lastError);});
 		});
 		
 	}
